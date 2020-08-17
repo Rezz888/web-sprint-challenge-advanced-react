a20600bb43e03815c70b749f51d7e505a37a0e05
Rezaur Rahman <rezaur.rahman_nyc@icloud.com>
2020-08-17 13:44:11 -0400

    Test added

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -5,10 +5,18 @@ import React, { useState } from "react";
 const useForm = (initialValue) => {
 
     const [values, setValues] = useState(initialValue);
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
 
-        
+    const handleChanges = (e) => {
+        setValues({ ...values, [e.target.name]: e.target.value });
+      };
 
-    return [values];
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        setShowSuccessMessage(true);
+      };   
+
+    return [values, handleChanges, handleSubmit, showSuccessMessage];
 }
 
 export default useForm;
